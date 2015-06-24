e9a66914b4bb6e5cf81d0f76c849aa907dbeaf24
Holger Schauf <holger.schauf@googlemail.com>
2015-06-24 17:40:02 +0200

    Create instal.sh

---
+++ instal.sh
@@ -0,0 +1,14 @@
+apm install linter
+npm i -g coffeelint eslint
+apm install atom-spotify2
+apm install color-picker
+apm install file-icons
+apm install git-history
+apm install grunt-runner
+apm install local-history
+apm install react
+apm install recent-files-fuzzy-finder
+apm install run-command
+apm install pigments
+apm install minimap
+apm install csslint
