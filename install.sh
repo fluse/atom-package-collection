579795a98f93787c54642281bdf67f124f030060
Holger Schauf <holger.schauf@googlemail.com>
2015-07-01 10:19:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 apm install linter
 npm i -g coffeelint eslint
-apm install atom-spotify2 color-picker file-icons git-history grunt-runner local-history react recent-files-fuzzy-finder run-command2 pigments minimap csslint nuclide-installer tree-view-breadcrumb
+apm install atom-spotify2 color-picker file-icons git-history grunt-runner local-history react recent-files-fuzzy-finder run-command2 pigments minimap csslint nuclide-installer tree-view-breadcrumb autocomplete-modules project-manager
